9c2f0c70ec7cc466f31caa07c1c17fb36a4ea2e1
fras hidayat <frashidayat@gmail.com>
2015-05-10 20:03:36 +0700

    update apps and delete  md bab

--- eJuklakApps/src/com/example/ejuklakapps/MyWebFragment.java
+++ eJuklakApps/src/com/example/ejuklakapps/MyWebFragment.java
@@ -12,45 +12,59 @@ import android.webkit.WebViewClient;
 
 public class MyWebFragment extends Fragment {
 
-    ProgressDialog mProgress;
-    WebView webview;
+	ProgressDialog mProgress;
+	WebView webview;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.web_fragment, container,
-                false);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.web_fragment, container,
+				false);
+		Bundle bundle = getArguments();
+		String url = bundle.getString("url");
+		webview = (WebView) rootView.findViewById(R.id.webview1);
 
-        Bundle bundle = getArguments();
-        String url = bundle.getString("url");
-        webview = (WebView) rootView.findViewById(R.id.webview1);
+		WebSettings settings = webview.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setBuiltInZoomControls(true);
 
-        WebSettings settings = webview.getSettings();
-        settings.setJavaScriptEnabled(true);
+		mProgress = ProgressDialog.show(getActivity(), "Loading",
+				"Please wait for a moment...");
 
-        mProgress = ProgressDialog.show(getActivity(), "Loading",
-                "Please wait for a moment...");
+		webview.loadUrl(url);
 
-        webview.loadUrl(url);
+		webview.setWebViewClient(new WebViewClient() {
 
-        webview.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				webview.loadUrl(url);
+				return true;
+			}
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				if (mProgress.isShowing()) {
+					mProgress.dismiss();
+				}
+			}
+			/*
+			 * public void onReceivedError(WebView view, int errorCode, String
+			 * description, String failingUrl) { int found =
+			 * failingUrl.indexOf('#');
+			 * 
+			 * if (found > 0) { anchor = failingUrl.substring(found + 1,
+			 * failingUrl.length()); view.loadUrl(failingUrl.substring(0,
+			 * found)); } }
+			 * 
+			 * public void onPageFinished(WebView view, String url) { if
+			 * (this.anchor != null) {
+			 * view.loadUrl("javascript:window.location.hash='" + this.anchor +
+			 * "'"); this.anchor = null; } }
+			 */
+		});
 
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                if (mProgress.isShowing()) {
-                    mProgress.dismiss();
-                }
-            }
-        });
-
-        return rootView;
-    }
+		return rootView;
+	}
 
 }
